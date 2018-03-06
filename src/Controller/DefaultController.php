35116eabb5b168816ca6faf9c0900f4e0615d70e
Adrien Merignac <adrien.merignac92@gmail.com>
2018-03-06 19:25:55 +0100

    Ajout des pages de liste des comics et de leurs détails + création d'un FavorisService

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,14 +5,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Cookie;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 use App\Services\MarvelService;
-use App\Entities\Favoris;
-
+use App\Services\FavorisService;
 
 class DefaultController extends Controller
 {
@@ -26,9 +24,15 @@ class DefaultController extends Controller
      */
     private $service;
 
-    public function __construct(MarvelService $service) {
+    /**
+     * Favoris Service
+     */
+    private $favorisService;
+
+    public function __construct(MarvelService $service, FavorisService $favorisService) {
         $this->service = $service;
         $this->limit = 20;
+        $this->favorisService = $favorisService;
     }
 
     /**
@@ -38,12 +42,7 @@ class DefaultController extends Controller
     public function index(Request $request, $page = 6)
     {
         // Création d'une liste de favoris vide par défaut
-        $favoris = [];
-
-        // Vérification de l'existance du cookie favoris
-        if($request->cookies->get('favoris') !== null) {
-            $favoris = unserialize($request->cookies->get('favoris'));
-        }
+        $favoris = $this->favorisService->getFavoris($request, "characters");
 
         // Gestion de la pagination (par défaut page 6)
         $page = $page < 1 ? 6 : $page;
@@ -74,7 +73,7 @@ class DefaultController extends Controller
      * @Method({"GET"})
      */
     public function detail(Request $request, $id)
-    {   
+    {
         $characters = $this->service->get("characters/".$id);
         $comics = $this->service->get("characters/".$id."/comics", [
             "orderBy" => "onsaleDate",
@@ -87,39 +86,99 @@ class DefaultController extends Controller
     }
 
     /**
+     * @Route("/comic/{page}", name="comics_list", requirements={"page" = "\d+"})
+     * @Template("list/comic.html.twig")
+     */
+    public function comics(Request $request, $page = 8)
+    {
+        $favoris = $this->favorisService->getFavoris($request, "comics");
+
+
+        // Gestion de la pagination (par défaut page 6)
+        $page = $page < 1 ? 6 : $page;
+        $offset = ($page - 1) * $this->limit;
+
+        // Appel de l'API Characters
+        $response = $this->service->get("comics", [
+            "offset" => $offset,
+            "limit" => $this->limit
+        ]);
+
+        return array(
+            "comics" => $response->data->results,
+            "page" => intval($page),
+            "totalPages" => round($response->data->total / $this->limit),
+            "first" => $offset === 0,
+            "last" => $response->data->count !== $this->limit,
+            "firstOffset" => $offset + 1,
+            "lastOffset" => $offset + $response->data->count,
+            "totalResults" => $response->data->total,
+            "favoris" => $favoris
+        );
+    }
+
+    /**
+     * @Route("/comic/{id}/", requirements={"id" = "\d+"}, name="comics_detail")
+     * @Template("detail/comicdetail.html.twig")
+     * @Method({"GET"})
+     */
+    public function comicDetail(Request $request, $id)
+    {
+        $comics = $this->service->get("comics/".$id);
+        $creators = $this->service->get("comics/".$id."/creators");
+        return array(
+            "comic" => $comics->data->results[0],
+            "creators" => $creators->data->results
+        );
+    }
+
+    /**
      * @Route("/details/{id}/favoris", requirements={"id" = "\d+"}, name="characters_detail_favoris")
      * @Method({"POST"})
      */
-    public function favoris(Request $request, $id)
+    public function favoris(Request $request)
     {
+        $name = "characters";
+
         // Création de la réponse
         $response = new Response();
 
         // Création d'une liste de favoris vide par défaut
-        $favorisList = [];
-
-        // Vérification de l'existance du cookie favoris
-        if($request->cookies->get('favoris') !== null) {
-            $favorisList = unserialize($request->cookies->get('favoris'));
-        }
+        $favorisList = $this->favorisService->getFavoris($request, $name);
 
         // Vérification du nombre de favoris (Maximum 5)
-        if(count($favorisList) > 4) {
-            // On renvoit le code HTTP 403 (Forbidden)
-            $response->setStatusCode(Response::HTTP_FORBIDDEN);
-            return $response;
-        }
+        $this->favorisService->checkFavoritesSize($favorisList, 5);
 
         // Création d'un favoris
-        $favoris = new Favoris(intVal($request->request->get('id')), $request->request->get('name'));
+        $cookie = $this->favorisService->createFavoris($request, $favorisList, $name);
 
-        // Ajout du favoris dans la liste des favoris
-        array_push($favorisList, $favoris);
-
-        // Création d'un cookie avec la liste des favoris mise à jour
-        $cookie = new Cookie('favoris', serialize($favorisList));
-        
         // Envoi du cookie dans le header de la réponse 
+        $response->headers->setCookie($cookie);
+
+        return $response;
+    }
+
+    /**
+     * @Route("/comic/{id}/favoris", requirements={"id" = "\d+"}, name="comic_detail_favoris")
+     * @Method({"POST"})
+     */
+    public function comicsFavoris(Request $request)
+    {
+        $name = "comics";
+
+        // Création de la réponse
+        $response = new Response();
+
+        // Création d'une liste de favoris vide par défaut
+        $favorisList = $this->favorisService->getFavoris($request, $name);
+
+        // Vérification du nombre de favoris (Maximum 8)
+        $this->favorisService->checkFavoritesSize($favorisList, 8);
+
+        // Création d'un favoris
+        $cookie = $this->favorisService->createFavoris($request, $favorisList, $name);
+
+        // Envoi du cookie dans le header de la réponse
         $response->headers->setCookie($cookie);
 
         return $response;
---
+++ src/Services/FavorisService.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Services;
+
+use App\Entities\Favoris;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+class FavorisService {
+
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Récupération des favoris
+     *
+     * @param Request $request
+     * @param $name
+     * @return array|mixed
+     */
+    public function getFavoris(Request $request, $name)
+    {
+        // Création d'une liste de favoris vide par défaut
+        $favoris = [];
+
+        // Vérification de l'existance du cookie favoris
+        if ($request->cookies->get($name) !== null) {
+            $favoris = unserialize($request->cookies->get($name));
+        }
+        return $favoris;
+    }
+
+    /**
+     * Création d'un favoris et ajout à la liste
+     *
+     * @param Request $request
+     * @param $favorisList
+     * @param $name
+     * @return Cookie
+     */
+    public function createFavoris(Request $request, $favorisList, $name): Cookie
+    {
+        // Création d'un favoris
+        $favoris = new Favoris(intVal($request->request->get('id')), $request->request->get('name'));
+
+        // Ajout du favoris dans la liste des favoris
+        array_push($favorisList, $favoris);
+
+        // Création d'un cookie avec la liste des favoris mise à jour
+        $cookie = new Cookie($name, serialize($favorisList));
+        return $cookie;
+    }
+
+    /**
+     * Vérification du nombre maximum de favoris
+     *
+     * @param $favorisList
+     * @param $size
+     */
+    public function checkFavoritesSize($favorisList, $size): void
+    {
+        if (count($favorisList) >= $size) {
+            // On renvoit le code HTTP 403 (Forbidden)
+            throw new AccessDeniedHttpException("Limite de favoris atteinte");
+        }
+    }
+}
